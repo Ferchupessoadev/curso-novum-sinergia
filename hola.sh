bc0a6022fb9c89c2242fd00153642e089fe00e42
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-06-02 22:50:51 -0300

    añade cosas de ale

--- hola.sh
+++ hola.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Hola mundo"
-echo "la re cague"
-echo "la re cague"
-echo "la re cague"
-echo "la re cague"
-echo "la re cague"
+echo "la re cague, instala debian."
+echo "la re cague, instala debian."
+echo "la re cague, instala debian."
+echo "la re cague, instala debian."
+echo "la re cague, instala debian."
